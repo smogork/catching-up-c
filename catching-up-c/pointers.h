11a6db3072118b3d9ecc221ad26a1181e4889d47
smogork <79797876+smogork@users.noreply.github.com>
2025-03-01 12:01:55 +0100

    Prepare first task interface

---
+++ catching-up-c/pointers.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int add_integers(int lhs, int rhs);
+void add_integers2(int lhs, int rhs, int* result);
+void add_integers3(int* lhs, int rhs);
+void add_integers_broken(int lhs, int rhs);
---
+++ catching-up-c/primitives.h
@@ -0,0 +1,8 @@
+#pragma once
+
+int rand_int(int from, int to);
+long long int rand_long(long long int from, long long int to);
+float rand_float(float from, float to);
+double rand_double(double from, double to);
+unsigned char rand_byte();
+char rand_character();
